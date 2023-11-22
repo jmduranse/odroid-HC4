faee8239b20f2303ce2377c752539d147488d4ef
jmduranse <86153959+jmduranse@users.noreply.github.com>
2023-11-22 20:48:50 +0100

    Update spindown.sh

--- spindown/spindown.sh
+++ spindown/spindown.sh
@@ -2,42 +2,42 @@
 # Spindown the drives
 
 # Storage drives only
-DRIVE1=/dev/sda
-DRIVE2=/dev/sdb
-
-
+DRIVE1="/dev/sda"
+DRIVE2="/dev/sdb"
+mail="jmduran"
 CHECK_DRIVES=("$DRIVE1" "$DRIVE2")
 
 function spindown() {
-
-    if hdparm -C "$1" | grep -q "standby"
-    then
-	echo "Drive $1 is already in standby"
-    else
-	hdparm -y "$1"; echo "Spinning down: $1"
-    fi
+   local drive="$1"
+   if hdparm -C "$drive" | grep -q "standby"; then
+       echo "Drive $drive is already in standby"
+   else
+       if hdparm -y "$drive"; then
+           echo "Spinning down: $drive"
+           echo "$(date): Spun down $drive" >> /var/log/drive_spindown.log
+       else
+           echo "Error spinning down $drive"
+       fi
+   fi
 }
 
 
 function check_use() {
+   local drive="$1"
+   local fileuse
+   fileuse="$(lsof "$drive")"
 
-    local fileuse
-    fileuse="$(lsof "$1")"
-
-    if  [[ -n  "$fileuse" ]]
-    then
-        mail -s "Unable to spin down disk $1"  jmduran <<<"$fileuse"
-	return 1
-
-    else
-	return 0
-    fi
+   if [[ -n "$fileuse" ]]; then
+       echo "Unable to spin down disk $drive"
+       mail -s "Unable to spin down disk $drive" $mail <<<"The following processes are using the disk: $fileuse"
+       return 1
+   else
+       return 0
+   fi
 }
-
-
-sync
+sync #probably unnecesary
 
 # Spin down drives
 for d in "${CHECK_DRIVES[@]}"; do
-    spindown "$d"
+  check_use "$d" && spindown "$d"
 done
